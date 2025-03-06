d5a8a68d4935207fcd2fba81de31e2f882a8787d
Ghasem Nikzad <99182405+QasemNik@users.noreply.github.com>
2025-03-06 16:40:34 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-// Import the fetchData function from the httpReq.js file
 import { fetchData } from "./utils/httpReq.js";
 
-// Get the DOM elements
+
 const resultElem = document.getElementById("result");
 const searchBtn = document.getElementById("search-btn");
 const input = document.getElementById("inp-word");
 let audioSrc = new Audio(); // Changed to create a new Audio object
 
-// Create a button element for playing sound
+
 let btn = document.createElement("button");
 btn.classList.add("btn");
 btn.innerHTML = `<i class="fas fa-volume-up"></i>`;
 
 // Function to play sound
 const playSound = (words, phoneticIndex) => {
-  // اگر phoneticIndex معتبر نیست، از مقدار پیش‌فرض 0 استفاده می‌کنیم
+ 
   let validIndex =
     phoneticIndex >= 0 && phoneticIndex <= words[0].phonetics.length
       ? phoneticIndex
       : 0;
-  audioSrc.src = words[0].phonetics[validIndex].audio; // تنظیم منبع صوتی با استفاده از ایندکس معتبر
-  resultElem.appendChild(btn); // اضافه کردن دکمه به المنت نتیجه
-  btn.onclick = () => audioSrc.play(); // اضافه کردن رویداد کلیک برای پخش صوت
+  audioSrc.src = words[0].phonetics[validIndex].audio;
+  resultElem.appendChild(btn);
+  btn.onclick = () => audioSrc.play(); 
 };
 
 // Function to display words and their details
 function displayWords(words) {
-  resultElem.innerHTML = ""; // Clear the result element
+  resultElem.innerHTML = "";
   words.forEach((i) => {
     const jsx = `
       <div class="word">
@@ -47,14 +46,14 @@ function displayWords(words) {
   });
 }
 
-// Handler for the search button click event
+
 const searchHandler = async () => {
   const inputValue = input.value;
   let response = await fetchData(inputValue);
   console.log(response);
   displayWords(response);
-  playSound(response, 2); // Assuming the audio URL is in the first phonetic object
+  playSound(response, 2); 
 };
 
-// Add event listener to the search button
+
 searchBtn.addEventListener("click", searchHandler);
